ddb4cf6dcbe110d6ef55d2e4ec81bb33f6444e3e
der1x <130235333+Mark4353@users.noreply.github.com>
2025-03-21 18:10:55 +0200

    hw-10

---
+++ main.js
@@ -0,0 +1,32 @@
+let count = 0;
+const maxCount = 5;
+
+const intervalId = setInterval(()=> {
+    count++;
+    console.log(`${count}` );
+    if (count >= maxCount){
+        clearInterval(intervalId);
+    }
+}, 1000) ;
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const box = document.querySelector(".animated-box");
+    let position = 0;
+    let size = 50 ;
+    const maxPosition = window.innerWidth - 50;
+    const intervalId = setInterval(() => {
+        position += 10;
+        size += 2;
+        box.style.left= position + "px" ;
+        box.style.width = size + "px" ;
+        box.style.height = size + "px" ;
+        
+        if (position >= maxPosition){
+            clearInterval(intervalId);
+            console.log("Анімація завершена.");
+        }
+    },100);
+}) ;
